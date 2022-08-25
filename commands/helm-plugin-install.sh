fe9c365d2f290ef73bdad0aa9ef582eab3e744ce
Markus Blaschke <mblaschke82@gmail.com>
2022-08-25 16:27:04 +0200

    fix plugin installation
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- commands/helm-plugin-install.sh
+++ commands/helm-plugin-install.sh
@@ -7,8 +7,10 @@ HOST_OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 HOST_ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
 PLUGIN_DOWNLOAD_FILE="helm-azure-tpl.${HOST_OS}.${HOST_ARCH}"
 PLUGIN_DOWNLOAD_URL="https://github.com/webdevops/helm-azure-tpl/releases/download/${HELM_AZURE_TPL_VERSION}/${PLUGIN_DOWNLOAD_FILE}"
+PLUGIN_TARGET_PATH="${HELM_PLUGIN_DIR}/${PLUGIN_DOWNLOAD_FILE}"
 
 echo "download from: $PLUGIN_DOWNLOAD_URL"
-echo "target path: ${HELM_PLUGIN_DIR}/${PLUGIN_DOWNLOAD_FILE}"
+echo "target path: $PLUGIN_TARGET_PATH"
 
-curl "$PLUGIN_DOWNLOAD_URL" -o "${HELM_PLUGIN_DIR}/${PLUGIN_DOWNLOAD_FILE}"
+curl --location "$PLUGIN_DOWNLOAD_URL" -o "$PLUGIN_TARGET_PATH"
+chmod +x -- "$PLUGIN_TARGET_PATH"
