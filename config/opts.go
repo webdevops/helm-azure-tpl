a7cf4f29748c17885abe711e222de3d355c088eb
Markus Blaschke <mblaschke82@gmail.com>
2022-08-25 16:32:25 +0200

    fix opts
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- config/opts.go
+++ config/opts.go
@@ -17,8 +17,8 @@ type (
 
 		// Api option
 		Azure struct {
-			Tenant      *string `long:"azuretpl.tenant"                   env:"AZURE_TENANT_ID"           description:"Azure tenant id" required:"true"`
-			Environment *string `long:"azuretpl.environment"              env:"AZURE_ENVIRONMENT"         description:"Azure environment name" default:"AzurePublicCloud"`
+			Tenant      *string `long:"azure.tenant"                   env:"AZURE_TENANT_ID"           description:"Azure tenant id" required:"true"`
+			Environment *string `long:"azure.environment"              env:"AZURE_ENVIRONMENT"         description:"Azure environment name" default:"AzurePublicCloud"`
 		}
 
 		DryRun bool `long:"dry-run"      env:"DRY_RUN"  description:"dry run"`
