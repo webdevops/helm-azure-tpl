527d03090164a7c1811d25f1af411eeec8f5b988
Markus Blaschke <mblaschke82@gmail.com>
2022-08-26 12:34:08 +0200

    improve plugin install
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- commands/helm-plugin-install.sh
+++ commands/helm-plugin-install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+set -o pipefail
 
 HELM_AZURE_TPL_VERSION=$(sed -n -e 's/version:[ "]*\([^"]*\).*/\1/p' "${HELM_PLUGIN_DIR}/plugin.yaml")
 
@@ -16,12 +17,20 @@ PLUGIN_DOWNLOAD_FILE="helm-azure-tpl.${HOST_OS}.${HOST_ARCH}"
 PLUGIN_DOWNLOAD_URL="https://github.com/webdevops/helm-azure-tpl/releases/download/${HELM_AZURE_TPL_VERSION}/${PLUGIN_DOWNLOAD_FILE}"
 PLUGIN_TARGET_PATH="${HELM_PLUGIN_DIR}/${PLUGIN_DOWNLOAD_FILE}"
 
-echo "download from: $PLUGIN_DOWNLOAD_URL"
-echo "target path: $PLUGIN_TARGET_PATH"
+echo "binary download from: $PLUGIN_DOWNLOAD_URL"
+echo "binary target path:   $PLUGIN_TARGET_PATH"
 
 curl --location "$PLUGIN_DOWNLOAD_URL" -o "$PLUGIN_TARGET_PATH"
-if [ "$?" -nq 0 ];
+if [ "$?" -nq 0 ]; then
     >&2 echo "[ERROR] failed to download plugin binary"
     exit 1
 fi
+
+if [[ ! -f  "$PLUGIN_TARGET_PATH" ]]; then
+    >&2 echo "[ERROR] installation of binary failed, please report issue"
+    exit 1
+fi
+
 chmod +x "$PLUGIN_TARGET_PATH"
+
+echo "successfully downloaded binary"
