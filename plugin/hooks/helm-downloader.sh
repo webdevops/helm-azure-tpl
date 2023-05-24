c9d3e75f847241647c9ba14b1403b237358c7bc1
Markus Blaschke <mblaschke82@gmail.com>
2023-05-25 00:22:32 +0200

    improve log output for downloader mode (azuretpl://)
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- plugin/hooks/helm-downloader.sh
+++ plugin/hooks/helm-downloader.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-exec "$HELM_BIN" azure-tpl apply --stdout "$4"
+set -e
+set -o pipefail
+
+>&2 echo ""
+>&2 echo "executing azure-tpl for \"$4\":"
+"$HELM_BIN" azure-tpl apply --stdout "$4"
+EXIT_CODE="$?"
+>&2 echo ""
+exit "$EXIT_CODE"
