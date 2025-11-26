c50cdd8a54a1cbac5b685ffa96dc448f60239534
Markus Blaschke <mblaschke82@gmail.com>
2025-11-26 18:30:00 +0100

    fix downloader (trigger script)
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- helm-downloader.sh
+++ helm-downloader.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 >&2 echo ""
 >&2 echo "executing azure-tpl for \"$4\":"
-./azure-tpl apply --stdout "$4"
+"${HELM_PLUGIN_DIR}/helm-azure-tpl" apply --stdout "$4"
 EXIT_CODE="$?"
 >&2 echo ""
 exit "$EXIT_CODE"
